6935bb32da9db0abab5638cd5ed18934d0914f41
Pam Gaiguer <pamella.gaiguer@gmail.com>
2024-03-30 02:13:29 -0300

    soon page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#002455",
+        btn: "#003D90",
+      },
+      fontFamily: {
+        "inter-regular": ["Inter", "sans-serif"],
+      },
+    },
     container: {
       center: true,
     },
