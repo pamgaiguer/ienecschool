d5fe1d156abf83c9dd109b350565ff219e0f247f
Pam Gaiguer <pamella.gaiguer@gmail.com>
2024-06-03 15:08:13 -0300

    adding components folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,29 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
-import { SoonComponent } from "./soon/soon.component";
-import { AdmissaoComponent } from "./admissao/admissao.component";
-import { CorpoDocenteComponent } from "./corpo-docente/corpo-docente.component";
-import { ProgramaEscolarComponent } from "./programa-escolar/programa-escolar.component";
-import { GaleriaFotosComponent } from "./galeria-fotos/galeria-fotos.component";
-import { SobreNosComponent } from "./sobre-nos/sobre-nos.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SoonComponent } from './soon/soon.component';
+import { AdmissaoComponent } from './admissao/admissao.component';
+import { CorpoDocenteComponent } from './corpo-docente/corpo-docente.component';
+import { ProgramaEscolarComponent } from './programa-escolar/programa-escolar.component';
+import { GaleriaFotosComponent } from './galeria-fotos/galeria-fotos.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "home", pathMatch: "full" },
-  { path: "home", component: HomeComponent },
-  { path: "soon", component: SoonComponent },
-  { path: "matriculas", component: AdmissaoComponent },
-  { path: "corpo-docente", component: CorpoDocenteComponent },
-  { path: "programa-escolar", component: ProgramaEscolarComponent },
-  { path: "galeria", component: GaleriaFotosComponent },
-  { path: "admissao", component: AdmissaoComponent },
-  { path: "sobre-nos", component: SobreNosComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'soon', component: SoonComponent },
+  { path: 'matriculas', component: AdmissaoComponent },
+  { path: 'corpo-docente', component: CorpoDocenteComponent },
+  { path: 'programa-escolar', component: ProgramaEscolarComponent },
+  { path: 'galeria', component: GaleriaFotosComponent },
+  { path: 'admissao', component: AdmissaoComponent },
+  { path: 'sobre-nos', component: SobreNosComponent },
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      anchorScrolling: "enabled",
+      anchorScrolling: 'enabled',
       useHash: false,
     }),
   ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
-import { SoonComponent } from "./soon/soon.component";
-import { NavbarComponent } from "./shared/navbar/navbar.component";
-import { FooterComponent } from "./shared/footer/footer.component";
-import { CorpoDocenteComponent } from "./corpo-docente/corpo-docente.component";
-import { GaleriaFotosComponent } from "./galeria-fotos/galeria-fotos.component";
-import { AdmissaoComponent } from "./admissao/admissao.component";
-import { ProgramaEscolarComponent } from "./programa-escolar/programa-escolar.component";
-import { SobreNosComponent } from "./sobre-nos/sobre-nos.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponentsModule } from '../components/components.module';
+import { HomeComponent } from './home/home.component';
+import { SoonComponent } from './soon/soon.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CorpoDocenteComponent } from './corpo-docente/corpo-docente.component';
+import { GaleriaFotosComponent } from './galeria-fotos/galeria-fotos.component';
+import { AdmissaoComponent } from './admissao/admissao.component';
+import { ProgramaEscolarComponent } from './programa-escolar/programa-escolar.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,7 @@ import { SobreNosComponent } from "./sobre-nos/sobre-nos.component";
     ProgramaEscolarComponent,
     SobreNosComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ComponentsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
