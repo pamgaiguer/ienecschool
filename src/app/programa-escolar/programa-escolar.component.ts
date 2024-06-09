95b06e712c90d715f36bdea1f35d258ce6340731
Pam Gaiguer <pamella.gaiguer@gmail.com>
2024-06-08 22:01:39 -0300

    adding programa escolar

--- src/app/programa-escolar/programa-escolar.component.ts
+++ src/app/programa-escolar/programa-escolar.component.ts
@@ -1,8 +1,55 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-programa-escolar",
-  templateUrl: "./programa-escolar.component.html",
-  styleUrls: ["./programa-escolar.component.scss"],
+  selector: 'app-programa-escolar',
+  templateUrl: './programa-escolar.component.html',
+  styleUrls: ['./programa-escolar.component.scss'],
 })
-export class ProgramaEscolarComponent {}
+export class ProgramaEscolarComponent {
+  cards = [
+    {
+      name: 'Registro',
+      role: 'Entre em contato para que possamso registrar seu nome e qual o ano letivo de interesse',
+    },
+    {
+      name: 'Venha nos conhecer',
+      role: 'Agende uma visita para conhecer mais das nossas instalações, e nossos colaboradores para já saber quem vai cuidar de você!',
+    },
+    {
+      name: 'Faça a sua matrícula',
+      role: 'Depois nos te enviaremos todos os formularios e documentação necessária pra finalizar o processo!',
+    },
+  ];
+  faqs = [
+    {
+      title: 'Registro',
+      description:
+        'Entre em contato para que possamos registrar seu nome e qual o ano letivo de interesse',
+    },
+    {
+      title: 'Registro',
+      description:
+        'Entre em contato para que possamos registrar seu nome e qual o ano letivo de interesse',
+    },
+    {
+      title: 'Registro',
+      description:
+        'Entre em contato para que possamos registrar seu nome e qual o ano letivo de interesse',
+    },
+    {
+      title: 'Registro',
+      description:
+        'Entre em contato para que possamos registrar seu nome e qual o ano letivo de interesse',
+    },
+    {
+      title: 'Registro',
+      description:
+        'Entre em contato para que possamos registrar seu nome e qual o ano letivo de interesse',
+    },
+    {
+      title: 'Registro',
+      description:
+        'Entre em contato para que possamos registrar seu nome e qual o ano letivo de interesse',
+    },
+  ];
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        dark: '#111928',
         main: '#002455',
         btn: '#003D90',
         mainFontColor: '#1F2A37',
         primaryMainGrey: '#637381',
         grayGray7: '#CED4DA',
+        primaryTextColor: '#637381',
       },
       fontFamily: {
         'inter-regular': ['Inter', 'sans-serif'],
