5e01e5b2b4367f1e2cc599802a490ad876cde4db
Pam Gaiguer <pamella.gaiguer@gmail.com>
2024-06-08 21:35:25 -0300

    pagina corpo docente adicionada

--- src/app/corpo-docente/corpo-docente.component.ts
+++ src/app/corpo-docente/corpo-docente.component.ts
@@ -1,8 +1,23 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-corpo-docente",
-  templateUrl: "./corpo-docente.component.html",
-  styleUrls: ["./corpo-docente.component.scss"],
+  selector: 'app-corpo-docente',
+  templateUrl: './corpo-docente.component.html',
+  styleUrls: ['./corpo-docente.component.scss'],
 })
-export class CorpoDocenteComponent {}
+export class CorpoDocenteComponent {
+  cards = [
+    { name: 'Coriss Ambady', role: 'Diretora' },
+    { name: 'Glorius Cristian', role: 'Atendimento' },
+    { name: 'Glorius Cristian', role: 'Psicologo' },
+    { name: 'Nikolas Brooten', role: 'Gerente de atividades' },
+    { name: 'Nikolas Brooten', role: 'Gerente de atividades' },
+    { name: 'Nikolas Brooten', role: 'Gerente de atividades' },
+    { name: 'Nikolas Brooten', role: 'Gerente de atividades' },
+    { name: 'Nikolas Brooten', role: 'Gerente de atividades' },
+    { name: 'Nikolas Brooten', role: 'Gerente de atividades' },
+    { name: 'Nikolas Brooten', role: 'Gerente de atividades' },
+    { name: 'Nikolas Brooten', role: 'Gerente de atividades' },
+    { name: 'Nikolas Brooten', role: 'Gerente de atividades' },
+  ];
+}
