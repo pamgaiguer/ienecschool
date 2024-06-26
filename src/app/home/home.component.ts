377dc7a212d2ee6e111c55b16f05c82fc8fd8b98
Pam Gaiguer <pamella.gaiguer@gmail.com>
2024-06-25 22:26:29 -0300

    adding fotos for home carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,4 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  carouselImages: string[] = [
+    'assets/fotos/galeria-colegio.jpg',
+    'assets/fotos/galeria-ensino-medio-1.jpg',
+    'assets/fotos/galeria-ensino-medio-2.jpg',
+    'assets/fotos/galeria-ensino-medio-3.jpg',
+    'assets/fotos/galeria-fundI-1.jpg',
+    'assets/fotos/galeria-fundI-2.jpg',
+    'assets/fotos/galeria-fundI-3.jpg',
+    'assets/fotos/galeria-fundI-4.jpg',
+    'assets/fotos/galeria-fundII-1.jpg',
+    'assets/fotos/galeria-fundII-2.jpg',
+    'assets/fotos/galeria-fundII-3.jpg',
+    'assets/fotos/galeria-fundII-4.jpg',
+    'assets/fotos/galeria-fundII-5.jpg',
+    'assets/fotos/galeria-fundII-6.jpg',
+    'assets/fotos/galeria-infantil-1.jpg',
+    'assets/fotos/galeria-infantil-2.jpg',
+    'assets/fotos/galeria-infantil-3.jpg',
+    'assets/fotos/galeria-lobato1.jpg',
+    'assets/fotos/galeria-lobato2.jpg',
+    'assets/fotos/galeria-lobato3.jpg',
+  ];
+}
