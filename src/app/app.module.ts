1e9405048e7283ff201d020e5e9db9c95953f7aa
Pam Gaiguer <pamella.gaiguer@gmail.com>
2024-10-21 01:02:52 -0300

    adding manifest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +14,7 @@ import { AdmissaoComponent } from './admissao/admissao.component';
 import { DiferenciaisComponent } from './diferenciais/diferenciais.component';
 import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,12 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
     SobreNosComponent,
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ComponentsModule],
+  imports: [BrowserModule, AppRoutingModule, ComponentsModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [provideHttpClient(withInterceptorsFromDi())],
 })
 export class AppModule {}
