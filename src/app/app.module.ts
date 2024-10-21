e801d2e682846061fd2b098712f517536d663ad2
Pam Gaiguer <pamella.gaiguer@gmail.com>
2024-10-21 01:17:08 -0300

    preparing for deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,12 +30,17 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     SobreNosComponent,
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ComponentsModule, ServiceWorkerModule.register('ngsw-worker.js', {
-  enabled: !isDevMode(),
-  // Register the ServiceWorker as soon as the application is stable
-  // or after 30 seconds (whichever comes first).
-  registrationStrategy: 'registerWhenStable:30000'
-})],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ComponentsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   providers: [provideHttpClient(withInterceptorsFromDi())],
 })
 export class AppModule {}
