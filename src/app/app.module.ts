83c579cffd2effe6049ebf76655d95be9f172b3a
Pam Gaiguer <pamella@quantiz.com.br>
2024-10-29 19:05:48 -0300

    updating pre firebae module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
