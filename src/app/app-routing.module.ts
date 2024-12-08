e318a51e00640240acf94b5511cbfb17396c7dc0
Pam Gaiguer <pamella.gaiguer@gmail.com>
2024-12-08 13:23:41 -0300

    updating navbar menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,19 +5,14 @@ import { SoonComponent } from './soon/soon.component';
 import { AdmissaoComponent } from './admissao/admissao.component';
 import { MetodologiaEnsinoComponent } from './metodologia-ensino/metodologia-ensino.component';
 import { DiferenciaisComponent } from './diferenciais/diferenciais.component';
-import { GaleriaFotosComponent } from './galeria-fotos/galeria-fotos.component';
-import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'soon', component: SoonComponent },
-  { path: 'matriculas', component: AdmissaoComponent },
   { path: 'metodologias', component: MetodologiaEnsinoComponent },
   { path: 'diferenciais', component: DiferenciaisComponent },
-  { path: 'galeria', component: GaleriaFotosComponent },
   { path: 'admissao', component: AdmissaoComponent },
-  { path: 'sobre-nos', component: SobreNosComponent },
 ];
 
 @NgModule({
