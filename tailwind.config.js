4e2d4125eb633be98e05b6289af4292d179055c2
Pam Gaiguer <pamella@quantiz.com.br>
2024-12-11 23:44:08 -0300

    replacing banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,23 @@ module.exports = {
   content: ['./src/**/*.{html,ts}', './node_modules/flowbite/**/*.js'],
   darkMode: 'class',
   theme: {
-    extend: {
-      colors: {
-        bgMain: '#f9f9f9',
-        dark: '#111928',
-        main: '#002455',
-        btn: '#003D90',
-        mainFontColor: '#1F2A37',
-        primaryMainGrey: '#637381',
-        grayGray7: '#CED4DA',
-        primaryTextColor: '#637381',
-        blueDefault: '#003D90',
-        redDefault: '#EB0006',
-        yellowDefault: '#FEC401',
-      },
-      fontFamily: {
-        'inter-regular': ['Inter', 'sans-serif'],
-      },
+    colors: {
+      bgMain: '#f9f9f9',
+      dark: '#111928',
+      main: '#002455',
+      btn: '#003D90',
+      mainFontColor: '#1F2A37',
+      primaryMainGrey: '#637381',
+      grayGray7: '#CED4DA',
+      primaryTextColor: '#637381',
+      blueDefault: '#003D90',
+      redDefault: '#EB0007',
+      yellowDefault: '#FEC400',
     },
+    fontFamily: {
+      'inter-regular': ['Inter', 'sans-serif'],
+    },
+    extend: {},
     container: {
       center: true,
     },
