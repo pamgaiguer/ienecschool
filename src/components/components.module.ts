15dc19819491f8a65dd698b9536c94e8bb178b87
Pam Gaiguer <pamella.gaiguer@gmail.com>
2025-05-26 00:02:21 -0300

    fix: infantil photo name w/ capslock

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ContactFormComponent } from './contact-form/contact-form.component';
 import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
-  declarations: [ContactFormComponent, BannerComponent],
+  declarations: [BannerComponent],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [ContactFormComponent, BannerComponent],
+  exports: [BannerComponent],
 })
 export class ComponentsModule {}
